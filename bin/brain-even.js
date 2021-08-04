c41957e5295a07011fc7957eb4b4e504ce6a9222
Natalya Reva <ta_sh2003@mail.ru>
2021-08-04 17:26:35 +0300

    add file brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import greetings from '../src/greeting.js';
+import getUserName from '../src/cli.js';
+import questionAnswer from '../src/one.js';
+
+greetings();
+getUserName();
+questionAnswer();
+
+//const userName = getUserName();
+//console.log(`jhjhu, ${userName}`);
