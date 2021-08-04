70918fd00e5a2385d29b2c18d1ae4d386e12b46d
Natalya Reva <ta_sh2003@mail.ru>
2021-08-04 17:19:10 +0300

    add new files

--- src/cli.js
+++ src/cli.js
@@ -4,4 +4,6 @@ const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
 export default getUserName;
+
---
+++ src/greeting.js
@@ -0,0 +1,4 @@
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+};
+export default greetings;
