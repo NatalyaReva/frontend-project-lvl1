9a44f399020e6f7c44d44b1a46a64ace6241e9df
Natalya Reva <ta_sh2003@mail.ru>
2021-08-11 03:07:54 +0300

    add file gcd.js

---
+++ src/gcd.js
@@ -0,0 +1,34 @@
+import { getRandomNumber, getUserAnswer } from './one.js';
+
+// Нахождение НОД
+const gcd = (number1, number2) => {
+  if (number2 === 0) {
+    return number1;
+  }
+  return gcd(number2, number1 % number2);
+};
+
+const questionNod = (nameUser) => {
+  console.log('Find the greatest common divisor of given numbers.');
+  let i = 0;
+  for (i; i < 3; i += 1) {
+    const a = getRandomNumber();
+    const b = getRandomNumber();
+    const result = gcd(a, b);
+    const numbers = `${a} ${b}`;
+    console.log(`Question: ${numbers}`);
+    const answer = getUserAnswer();
+    if (Number(answer) === result) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer;(. Correct answer was ${result}`);
+      console.log(`Let's try  again, ${nameUser}!`);
+      break;
+    }
+  }
+  if (i > 2) {
+    console.log(`Congratulations, ${nameUser}!`);
+  }
+};
+
+export default questionNod;
