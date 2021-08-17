e7edd57c010ea9662f942c64de2a6f3534c09a46
Natalya Reva <ta_sh2003@mail.ru>
2021-08-17 23:40:13 +0300

    modified even.js

---
+++ src/even.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+
+//  Вывод рандомного числа
+const getRandomNumber = () => {
+  const randomNumber = Math.round(Math.random() * 101);
+  return randomNumber;
+};
+
+//  Получение ответа от пользователя
+const getUserAnswer = () => {
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  return userAnswer;
+};
+
+// Определение четное или нечетное число
+const evenOrOdd = (result) => {
+  if (result % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+// Вопрос-ответ с пользователем
+const questionAnswer = (nameUser) => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no" ');
+  let i = 0;
+  for (i; i < 3; i += 1) {
+    const result = getRandomNumber();
+    console.log(`Question: ${result}`);
+    const answer = getUserAnswer();
+    if (answer === evenOrOdd(result)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer;(. Correct answer was ${evenOrOdd(result)}`);
+      console.log(`Let's try  again, ${nameUser}`);
+      break;
+    }
+  }
+  if (i > 2) {
+    console.log(`Congratulations, ${nameUser}`);
+  }
+};
+export default questionAnswer;
