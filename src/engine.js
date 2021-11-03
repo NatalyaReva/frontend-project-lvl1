b728d81eaa1690b1fbfa7c39b664fbb12113d4d0
Natalya Reva <ta_sh2003@mail.ru>
2021-11-03 13:23:50 +0300

    modified engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,23 +1,23 @@
 import { greetings, getUserName, getUserAnswer } from './utils.js';
-import exercise, { startPlay } from './games/calc.js';
+import { startPlay, exercise } from './games/calc.js';
 
 export const startGame = (rule, getAnswer) => {
   greetings();
-  //getUserName();
-  const userName = 'Bob'; //getUserName();
+  // getUserName();
+  const userName = getUserName();
   // Задание
   console.log(exercise);
   // количество раундов
-  //const { question } = startPlay();
-  const res = startPlay();
-  const question = res.question;
-  const { correctAnswer } = res;
-  for (let i = 0; i <= 3; i += 1) {
-  // вопрос пользователю
+  // const { question } = startPlay();
+  for (let i = 1; i <= 3; i += 1) {
+    const res = startPlay();
+    const question = res.question;
+    const { correctAnswer } = res;
+    // вопрос пользователю
     console.log(`Question: ${question}`);
     // ответ пользователя
     const userAnswer = getUserAnswer();
-    if (userAnswer === correctAnswer) {
+    if (Number(userAnswer) === correctAnswer) {
       console.log('Correct!');
     } else {
       console.log(`${userAnswer} is wrong answer;(. Correct answer was ${correctAnswer}`);
