a034d8e6a53eb19d9b263b1b0385c0b76240ee08
Natalya Reva <ta_sh2003@mail.ru>
2021-11-05 16:34:16 +0300

    modified brain-evn.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-import greetings from '../src/greeting.js';
-import getUserName from '../src/cli.js';
-import questionAnswer from '../src/games/even.js';
+import { playGameEven } from '../src/games/even.js';
 
-greetings();
-questionAnswer(getUserName());
+playGameEven();
