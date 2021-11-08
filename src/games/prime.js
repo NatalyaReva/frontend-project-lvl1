2891af759b4dce241635759f0ae9c0f758f121df
Natalya Reva <ta_sh2003@mail.ru>
2021-11-08 03:32:15 +0300

    modified prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,36 +1,28 @@
-import { getRandomNumber, getUserAnswer } from './even.js';
+import { getRandomNum } from '../utils.js';
+import startGame from '../engine.js';
 
 // Определяю является ли число простым
 const getPrime = (num) => {
   if (num < 2) {
-    return 'no';
+    return false;
   }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
-// Вопрос-ответ с пользователем
-const questionAnswer = (nameUser) => {
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  let i = 0;
-  for (i; i < 3; i += 1) {
-    const result = getRandomNumber();
-    console.log(`Question: ${result}`);
-    const answer = getUserAnswer();
-    if (answer === getPrime(result)) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer;(. Correct answer was ${getPrime(result)}`);
-      console.log(`Let's try again, ${nameUser}!`);
-      break;
-    }
-  }
-  if (i > 2) {
-    console.log(`Congratulations, ${nameUser}!`);
-  }
+export const exercise = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const startPlay = () => {
+  const number = getRandomNum(0, 20);
+  const question = number;
+  const correctAnswer = getPrime(number) ? 'yes' : 'no';
+  return { question, correctAnswer };
 };
-export default questionAnswer;
+
+export const playGameIsPrime = () => {
+  startGame(exercise, startPlay);
+};
