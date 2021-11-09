44abadbb796a59784e1e46511d76008b81e0463f
Natalya Reva <ta_sh2003@mail.ru>
2021-11-10 02:59:21 +0300

    update engine.js

--- src/engine.js
+++ src/engine.js
@@ -16,13 +16,12 @@ const startGame = (exercise, startPlay) => {
     console.log(`Question: ${question}`);
     // ответ пользователя
     const userAnswer = getUserAnswer();
-    if (userAnswer.toString() === correctAnswer.toString()) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer.toString() !== correctAnswer.toString()) {
       console.log(`${userAnswer} is wrong answer;(. Correct answer was ${correctAnswer}`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
